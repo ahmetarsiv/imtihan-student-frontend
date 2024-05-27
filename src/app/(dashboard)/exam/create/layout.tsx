fe968802d1678271f26222d414843e0118d38a4e
Ali KARABAY <45515180+karabayyazilim@users.noreply.github.com>
2024-05-27 19:42:30 +0300

    custom exam page deleted.

--- src/app/(dashboard)/exam/create/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    title: 'Oluştur',
-};
-export default function NoteCreateLayout(props: { children: ReactNode }) {
-    return props.children;
-}

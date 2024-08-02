0fd2922452d820c47a97fca7c01458101d4351d6
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-08-02 16:49:00 +0300

    Password must be longer than 8 characters and must be configured for Sign in with Google configuration.

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -8,15 +8,12 @@ import React, { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Button, Input, Label } from '@codenteq/interfeys';
 import { useAuthContext } from '@/auth/hooks/useAuthContext';
+import GoogleAuthButton from '@/components/GoogleAuthButton';
 
 const Register = () => {
     const searchParams = useSearchParams();
     const [isLoading, setIsLoading] = useState(false);
     const { push } = useRouter();
-
-    const handleGoogleLogin = () => {
-        window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`;
-    };
 
     const { register, errorMessages } = useAuthContext();
 
@@ -65,27 +62,7 @@ const Register = () => {
                         Devam etmek için İmtihan'a kaydolun.
                     </p>
 
-                    <button onClick={handleGoogleLogin}>
-                        <div className="w-full border hover:border-brand dark:bg-[#fff] dark:hover:bg-[#e5e7eb]/90 rounded-full text-lg px-5 py-2.5 inline-flex items-center mb-2">
-                            <svg
-                                className="text-black mr-2 -ml-1 w-4 h-4"
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fab"
-                                data-icon="google"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 488 512">
-                                <path
-                                    fill="currentColor"
-                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                                />
-                            </svg>
-                            <span className="text-black text-center font-medium">
-                                Google ile kayıt olun.
-                            </span>
-                        </div>
-                    </button>
+                    <GoogleAuthButton label="Google ile kaydolun"/>
                 </div>
 
                 <div className="inline-flex justify-center items-center w-full">
@@ -136,6 +113,7 @@ const Register = () => {
                             onChange={event => setPassword(event.target.value)}
                             required
                             autoComplete="new-password"
+                            minLength={8}
                             messages={errorMessages?.password}
                         />
                     </div>
@@ -152,6 +130,7 @@ const Register = () => {
                                 setPasswordConfirmation(event.target.value)
                             }
                             required
+                            minLength={8}
                             messages={errorMessages?.password_confirmation}
                         />
                     </div>
